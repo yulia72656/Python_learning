38aee7d009f51b78480c411687946151748ef6d3
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-05-31 21:13:06 +0300

    calculated average squared

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import math
+def convert_rads_to_degrees(rad):
+    degree = (180 / math.pi) * rad
+    return degree
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def return_area(l, h):
+    area = (l * h)
+    return area
 
-
+def calculate_avsqrt(list):
+    summ = 0
+    for number in list:
+        summ = summ + number
+    av = summ / len(list)
+    summsqrt = 0
+    for number in list:
+        summsqrt = summsqrt + math.pow(number - av, 2)
+    avgsqrt = math.sqrt(summsqrt)
+    return avgsqrt
+#math.sqrt((x1 - xaver)^2 + (x2 - xaver)^2 + ... + (xn - aver)^2)
+#math.sqrt
+#math.pow(x, 2)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hi John. How are you?')
+    X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 123, 123, 11, 22, 22, 33, 44]
+    print(calculate_avsqrt(X))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
